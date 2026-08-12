c7bb185e8ceb0538f5e706da43cb6c2725617ca5
Lkgpw <sanibronx21@gmail.com>
2026-08-12 11:35:09 -0300

    Create web.py

---
+++ web.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app_web = Flask(__name__)
+
+@app_web.route('/')
+def home():
+    return "SanizinhaBot está online!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app_web.run(host="0.0.0.0", port=port)
