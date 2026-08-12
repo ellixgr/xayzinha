8ece4aa2e7388bf047d6c2640e9b5af2acc3778f
Lkgpw <sanibronx21@gmail.com>
2026-08-12 12:24:41 -0300

    Update home route message with emoji

--- web.py
+++ web.py
@@ -4,8 +4,4 @@ app_web = Flask(__name__)
 
 @app_web.route('/')
 def home():
-    return "SanizinhaBot está online!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app_web.run(host="0.0.0.0", port=port)
+    return "SanizinhaBot está online! 🟢"
